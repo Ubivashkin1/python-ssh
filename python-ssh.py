80d2766ec655444e26be2af85956685ff87414aa
ubivashkin1 <sb.op100500@gmail.com>
2019-06-28 10:25:31 +1000

    Changed how the timestamp is added to the config file
    Added comments to code

--- python-ssh.py
+++ python-ssh.py
@@ -1,20 +1,24 @@
-import datetime
+import time
 from netmiko import ConnectHandler
 
+# Test to make sure code initialises
 print("Before Config Push")
 
+# Initialise SSH connection to target device. Device/IP/Username/Password
 device = ConnectHandler(device_type="huawei", ip="10.100.20.99", username="admin", password="Password1")
 
+# Send desired command matching CLI of type/model of device
 output = device.send_command("dis ip int brief")
 
-#print(output)
+# Define Timestamp (data/time)
+timestr = time.strftime("%d%m%Y-%H%M%S")
 
-x = datetime.datetime.now()
+# Create file with timestamp as file name + device hostname
+f = open(str(timestr + "_ar1220_config.txt"), "w+")
 
-f = open(x.strftime("%B"), "w+")
-
+# Write command output contents to the newly created file in above line
 f.write(output)
 
+# Disconnect from ssh session to prevent hanging sessions
 device.disconnect()
 
-print("After Config Push")
